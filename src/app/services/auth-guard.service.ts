5f8497a16312bdb2c3c10d0be68fcf68000e7cc0
aalphones <sascha.mick@outlook.de>
2024-01-03 15:24:44 +0100

    update to v16

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import {
   retrieveSettings,
@@ -9,7 +9,7 @@ import {
 } from '../models/user-settings.model';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
   password = this.retrievePassword();
 
   get isAuthenticated(): boolean {
